911280772c2d94e4992d157e0813fe4260846936
Aparanjini06 <aparanjinipallela1@gmail.com>
2026-06-04 16:24:32 +0530

    feat: add reverse number program

---
+++ src/com/codegnan/fundmentals/Reverse.java
@@ -0,0 +1,20 @@
+package com.codegnan.fundmentals;
+
+import java.util.Scanner;
+
+public class Reverse {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter a number:");
+		int num = s.nextInt();
+		int temp = num;
+		int reverse = 0;
+		while (num != 0) {
+			int remainder = num % 10;
+			reverse = reverse * 10 + remainder;
+			num = num / 10;
+		}
+		System.out.println("reverse of a given number " + temp + " is:" + reverse);
+		s.close();
+	}
+}
