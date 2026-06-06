f7313085aa70ab95b271332e3d8a8498c9287c0b
Aparanjini06 <aparanjinipallela1@gmail.com>
2026-06-06 15:48:35 +0530

    Update HappyNumber

--- src/com/codegnan/fundmentals/HappyNumber.java
+++ src/com/codegnan/fundmentals/HappyNumber.java
@@ -6,13 +6,13 @@ public class HappyNumber {
 
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
-		int num = s.nextInt();
+		int num = s.nextInt();//9
 
 		while (num != 1 && num != 4) {
 			int sum = 0;
 			while (num != 0) {
 				int rem = num % 10;
-				sum += rem * rem;
+				sum += rem * rem;//81
 				num /= 10;
 
 			}
