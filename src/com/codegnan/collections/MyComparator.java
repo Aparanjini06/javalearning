6a7fd031c010623411d128f1254dc507e3bba3b3
Aparanjini06 <aparanjinipallela1@gmail.com>
2026-07-01 16:16:15 +0530

    feat:Day 31 add comparator

---
+++ src/com/codegnan/collections/MyComparator.java
@@ -0,0 +1,33 @@
+package com.codegnan.collections;
+
+import java.util.Comparator;
+
+public class MyComparator implements Comparator{
+	@Override
+	public int compare(Object o1,Object o2)
+	{
+		String s1=o1.toString();
+		String s2=o2.toString();
+		return -s1.compareTo(s2);
+		/*Integer i1=(Integer) o1;
+		Integer i2=(Integer) o2;
+		if(i1<i2)
+		{
+			return +1;
+		}
+		else
+		{
+			if(i1>i2)
+			{
+				return -1;
+			}
+			else
+			{
+				return 0;
+			}
+		}
+		return -i1.compareTo(i2);
+		*/
+		
+	}
+}
