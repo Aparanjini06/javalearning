88e2046b5356315ccc8f269d293828ed9664ecf0
Aparanjini06 <aparanjinipallela1@gmail.com>
2026-07-03 09:03:49 +0530

    feat(day-26): add custom thread by extending Thread class

---
+++ src/com/codegnan/multithreading/Mythread.java
@@ -0,0 +1,12 @@
+package com.codegnan.multithreading;
+
+public class Mythread extends Thread{
+	
+	public void run()
+	{
+		for(int i=0;i<10;i++)
+		{
+			System.out.println("Child Thread");
+		}
+	}
+}
