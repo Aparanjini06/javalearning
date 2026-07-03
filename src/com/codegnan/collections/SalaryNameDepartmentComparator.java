cbc6a03d93b901841040235cc8de1b516bd0a945
Aparanjini06 <aparanjinipallela1@gmail.com>
2026-07-03 08:58:00 +0530

    feat(day-31): add comparator for sorting employees by salary, name, and department

---
+++ src/com/codegnan/collections/SalaryNameDepartmentComparator.java
@@ -0,0 +1,35 @@
+package com.codegnan.collections;
+
+import java.util.Comparator;
+
+public class SalaryNameDepartmentComparator implements Comparator<Employee>{
+
+	@Override
+	public int compare(Employee e1, Employee e2) {
+		if(e1.getEmpSalary()<e2.getEmpSalary())
+		{
+			return -10;
+		}
+		else
+		{
+			if(e1.getEmpSalary()>e2.getEmpSalary())
+			{
+				return 10;
+			}
+			else
+			{
+				int result=e1.getEmpName().compareTo(e2.getEmpName());
+				if(result!=0)
+				{
+					return result;
+				}
+				else
+				{
+					return e1.getEmpAddress().compareTo(e2.getEmpAddress());
+					
+				}
+			}
+		}
+	}
+
+}
