0c55d3ade55ba25379a5bdbdb0eaaf6479adcf35
Aparanjini06 <aparanjinipallela1@gmail.com>
2026-07-03 09:08:30 +0530

    feat(day-16): add ContractEmployee class

---
+++ src/com/codegnan/oopprogramming/ContractEmployee.java
@@ -0,0 +1,14 @@
+package com.codegnan.oopprogramming;
+
+public class ContractEmployee extends Employee1{
+	double contractAmount;
+	public ContractEmployee(String name,double contractAmount) {
+		super(name);
+		this.contractAmount=contractAmount;
+	}
+	@Override
+	public double calculateSalary()
+	{
+		return contractAmount;
+	}
+}
