879d1eba9f97d6b91207b2bf67473669d1330031
Aparanjini06 <aparanjinipallela1@gmail.com>
2026-07-03 09:00:01 +0530

    feat(day-32): implement custom comparator for map sorting

---
+++ src/com/codegnan/map/MyComparator.java
@@ -0,0 +1,12 @@
+package com.codegnan.map;
+
+import java.util.Comparator;
+
+public class MyComparator  implements Comparator<Person>{
+
+	@Override
+	public int compare(Person p1, Person p2) {
+		return -p1.getEmail().compareTo(p2.getEmail());
+	}
+
+}
