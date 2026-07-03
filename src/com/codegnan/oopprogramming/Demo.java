06bddd49315978082a6822acc4f4acb942300bfe
Aparanjini06 <aparanjinipallela1@gmail.com>
2026-07-03 09:08:30 +0530

    feat(day-16): add polymorphism demo

---
+++ src/com/codegnan/oopprogramming/Demo.java
@@ -0,0 +1,15 @@
+package com.codegnan.oopprogramming;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		String s=new String("Codegnan");
+		s.concat(" Solutions");
+		System.out.println(s);
+		StringBuilder sb=new StringBuilder("codegnan");
+		sb.append(" Solutions");
+		
+		System.out.println(sb);
+	}
+
+}
