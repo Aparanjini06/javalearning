b40a064d048f9abeb2761f88ebcd359fd3c1582f
Aparanjini06 <aparanjinipallela1@gmail.com>
2026-07-03 09:06:42 +0530

    feat(day-20): add PasswordChecker interface

---
+++ src/com/codegnan/oopclass/PasswordChecker.java
@@ -0,0 +1,8 @@
+package com.codegnan.oopclass;
+
+public interface PasswordChecker {
+	public abstract String checkLenght(String password);
+
+	public abstract String checkComplexity(String password);
+	
+}
