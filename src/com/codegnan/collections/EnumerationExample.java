e4589959f6401f6f3c830e6010df496f92b741d3
Aparanjini06 <aparanjinipallela1@gmail.com>
2026-07-07 15:35:57 +0530

    feat(day-30): add EnumerationExample demonstrating Enumeration interface

---
+++ src/com/codegnan/collections/EnumerationExample.java
@@ -0,0 +1,21 @@
+package com.codegnan.collections;
+
+import java.util.Enumeration;
+import java.util.Vector;
+
+public class EnumerationExample {
+	public static void main(String[] args) {
+		Vector<Integer> v=new Vector<>();
+		for(int i=0;i<=10;i++)
+		{
+			v.add(i);
+		}
+		System.out.println(v);
+		Enumeration<Integer>e=v.elements();
+		while(e.hasMoreElements())
+			
+		{
+			System.out.println(e.nextElement());
+		}
+	}
+}
